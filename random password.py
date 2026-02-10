e8c62a72a82c9ae6d43d11377a46223039bf20bf
Nancy Sheokand <nancyshknd5@gmail.com>
2026-02-10 23:23:24 +0530

    Add files via upload

---
+++ random password.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+pass_len=8
+charvalues = string.ascii_letters + string.digits + string.punctuation
+
+password = "".join({random.choice(charvalues) for i in range(pass_len)})
+
+print("your random password is:", password)
